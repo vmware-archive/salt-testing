86b35bab41e375edd22ba409d6b840e6ada2b734
Pedro Algarvio <pedro@algarvio.me>
2013-08-26 09:18:32 +0000

    Provide a mock module that wraps the real one for cleaner handling when mock is actually not available.
    Because some unit tests use class/function decorators which require mock to be installed, there's no clean way to let the users know that mock is not available. `salttesting.mock` provides some fake objects for the most used `mock` objects that mocks the real one and allows proper skips.

---
+++ salttesting/mock.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+'''
+    salttesting.mock
+    ~~~~~~~~~~~~~~~~
+
+    Helper module that wraps `mock`_ and provides some fake objects in order to
+    properly set the function/class decorators and yet skip the test cases
+    execution.
+
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+'''
+
+try:
+    from mock import *
+    NO_MOCK = False
+    NO_MOCK_REASON = ''
+except ImportError:
+    NO_MOCK = True
+    NO_MOCK_REASON = 'mock python module is unavailable'
+
+    class MagicMock(object):
+
+        __name__ = '{0}.fakemock'.format(__name__)
+
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def dict(self, *args, **kwargs):
+            return self
+
+        def __call__(self, *args, **kwargs):
+            return self
+
+    Mock = MagicMock
+    patch = MagicMock()
+
+if NO_MOCK is False:
+    try:
+        from mock import call, ANY
+    except ImportError:
+        NO_MOCK = True
+        NO_MOCK_REASON = 'you need to upgrade your mock version to >= 0.8.0'
