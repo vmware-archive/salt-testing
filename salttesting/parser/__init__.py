c57c99f2c844cd036c20599045922ff380a4cb5a
Pedro Algarvio <pedro@algarvio.me>
2014-01-12 22:38:41 +0000

    Remove left over prints

--- salttesting/parser/__init__.py
+++ salttesting/parser/__init__.py
@@ -187,8 +187,6 @@ class SaltTestingParser(optparse.OptionParser):
         print_header(u'', inline=True)
         self.pre_execution_cleanup()
 
-        print 999
-
         if self.support_docker_execution and self.options.docked is not None:
             if self.source_code_basedir is None:
                 raise RuntimeError(
