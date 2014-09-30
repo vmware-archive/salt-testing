3f09629dca9e67a464a4845cd78c0efc7e9f0859
Pedro Algarvio <pedro@algarvio.me>
2014-09-30 17:48:00 +0100

    Pass output columns to runtests.py

--- salttesting/jenkins.py
+++ salttesting/jenkins.py
@@ -941,10 +941,11 @@ def main():
         options.test_command = (
             '{python_executable} /testing/tests/runtests.py -v --run-destructive --sysinfo'
             '{no_color} --ssh --xml=/tmp/xml-unitests-output --coverage-xml=/tmp/coverage.xml '
-            '--transport={transport}'.format(
+            '--transport={transport} --output-columns={output_columns}'.format(
                 python_executable=get_minion_python_executable(options),
                 no_color=options.no_color and ' --no-color' or '',
-                transport=options.test_transport
+                transport=options.test_transport,
+                output_columns=options.output_columns
             )
         )
     if options.test_command:
