ec903c1b5dacceae108ba3893394a27e69f3dafd
Pedro Algarvio <pedro@algarvio.me>
2014-12-16 11:54:00 +0000

    Add a file permission pylint checker

---
+++ salttesting/pylintplugins/fileperms.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+import stat
+from pylint.interfaces import IRawChecker
+from pylint.checkers import BaseChecker
+
+
+class FilePermsChecker(BaseChecker):
+    '''
+    Check for files with undesirable permissions
+    '''
+
+    __implements__ = IRawChecker
+
+    name = 'fileperms'
+    msgs = {'E0599': ('Module file has the wrong file permissions: %s',
+                      'file-perms',
+                      ('Wrong file permissions')),
+           }
+
+    priority = -1
+
+    options = (('fileperms_default',
+                {'default': '0644', 'type': 'string', 'metavar': 'ZERO_PADDED_PERM',
+                 'help': 'Desired file permissons. Default: 0644'}
+               ),
+               ('fileperms_ignore_paths',
+                {'default': (), 'type': 'csv', 'metavar': '<comma-separated-list>',
+                 'help': 'File paths to ignore file permission. Glob patterns allowed.'}
+               )
+              )
+
+    def process_module(self, node):
+        '''
+        process a module
+        '''
+
+        for listing in glob.glob(self.config.fileperms_ignore_paths):
+            if node.file.split('{0}/'.format(os.getcwd()))[-1] in listing:
+                # File is ignored, no checking should be done
+                return
+
+        desired_perm = self.config.fileperms_default
+        desired_perm = desired_perm.strip('"').strip('\'').lstrip('0').zfill(4)
+        if desired_perm[0] != '0':
+            # Always include a leading zero
+            desired_perm = '0{0}'.format(desired_perm)
+
+        module_perms = str(oct(stat.S_IMODE(os.stat(node.file).st_mode)))
+        if module_perms != desired_perm:
+            self.add_message('E0599', line=1, args=module_perms)
+
+
+def register(linter):
+    '''
+    required method to auto register this checker
+    '''
+    linter.register_checker(FilePermsChecker(linter))
