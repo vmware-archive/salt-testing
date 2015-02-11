5833380fa70762107aa6200f17a8dbf217a696bb
Pedro Algarvio <pedro@algarvio.me>
2015-02-11 01:48:32 +0000

    Bump version

--- salttesting/version.py
+++ salttesting/version.py
@@ -4,9 +4,9 @@
     ~~~~~~~~~~~~~~~~~~~
 
     :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
-    :copyright: © 2013 by the SaltStack Team, see AUTHORS for more details.
+    :copyright: © 2013-2015 by the SaltStack Team, see AUTHORS for more details.
     :license: Apache 2.0, see LICENSE for more details.
 '''
 
-__version_info__ = (2014, 8, 5)
+__version_info__ = (2015, 2, 15)
 __version__ = '.'.join(map(str, __version_info__))
