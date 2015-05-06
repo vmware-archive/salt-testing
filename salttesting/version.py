6649d7576e259cd769d722e69c68edc7a868e7ea
Pedro Algarvio <pedro@algarvio.me>
2015-05-06 10:40:25 +0100

    Format instead of map

--- salttesting/version.py
+++ salttesting/version.py
@@ -12,4 +12,4 @@
 from __future__ import absolute_import
 
 __version_info__ = (2015, 2, 16)
-__version__ = '.'.join(map(str, __version_info__))
+__version__ = '{0}.{1}.{2}'.format(*__version_info__)
