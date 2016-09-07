a7b14572e0f25b571903d4745ffab2153c718435
Pedro Algarvio <pedro@algarvio.me>
2016-09-07 12:40:57 +0100

    Bump version for release

--- salttesting/version.py
+++ salttesting/version.py
@@ -11,5 +11,5 @@
 # Import Python Libs
 from __future__ import absolute_import
 
-__version_info__ = (2016, 8, 22)
+__version_info__ = (2016, 9, 7)
 __version__ = '{0}.{1}.{2}'.format(*__version_info__)
