3cd41af906b5b7e8b41886cf529b038a2e24de1a
Pedro Algarvio <pedro@algarvio.me>
2017-02-22 14:06:59 +0000

    Py3 compat

--- salttesting/xmlunit.py
+++ salttesting/xmlunit.py
@@ -39,32 +39,9 @@ try:
             self._captured = StringIO()
             self.delegate = delegate
 
-        def _get_encodings(self):
-            if hasattr(self, '_encodings'):
-                return self._encodings
-            encodings = []
-            loc_enc = locale.getdefaultlocale()[-1]
-            if loc_enc:
-                encodings.append(loc_enc)
-            def_enc = sys.getdefaultencoding()
-            if def_enc not in encodings:
-                encodings.append(def_enc)
-            if 'utf-8' not in encodings:
-                encodings.append('utf-8')
-            self._encodings = encodings
-            return encodings
-
         def write(self, text):
-            if not isinstance(text, six.text_type):
-                log.debug('Converting non unicode text into unicde')
-                for enc in self._get_encodings():
-                    try:
-                        text = text.decode(enc)
-                        break
-                    except UnicodeDecodeError:
-                        continue
-            if isinstance(text, six.text_type):
-                text = text.encode('utf-8')
+            if six.PY2 and isinstance(text, six.text_type):
+                text = text.encode(__salt_system_encoding__)
             self._captured.write(text)
             self.delegate.write(text)
 
